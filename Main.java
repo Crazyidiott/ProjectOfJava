f2458f8805d4ddff7cdd8a4dd94d032c07a23f82
Crazyidiott <90187982+Crazyidiott@users.noreply.github.com>
2021-12-06 16:51:17 +0800

    Update Main.java
    嘻嘻

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
+import model.ImageValue;
 import view.GameFrame;
 
 import javax.swing.*;
 
 public class Main{
     public static void main(String[] args) {
+        ImageValue.init();
         SwingUtilities.invokeLater(() -> {
 
-            GameFrame mainFrame = new GameFrame(800);
+            GameFrame mainFrame = new GameFrame(700);
             mainFrame.setVisible(true);
         });
     }

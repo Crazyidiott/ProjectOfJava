b208d4db0ef4c65e1c30a67de4af41661d2e98e2
QinYuzhou <90183764+QinYuzhou@users.noreply.github.com>
2021-12-08 15:06:47 +0800

    Create MenuFrame.java

---
+++ view/MenuFrame.java
@@ -0,0 +1,55 @@
+package view;
+
+import controller.GameController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MenuFrame extends JFrame
+{
+    private GameFrame gameFrame;
+    public MenuFrame(int frameSize){
+        this.setTitle("Menu");
+        this.setLayout(null);
+
+        Insets insets=this.getInsets();
+        this.setSize(frameSize+insets.left+insets.right,frameSize+insets.top+insets.bottom);
+
+        this.setLocationRelativeTo(null);
+
+        JButton normalModeBtn=new JButton();
+        normalModeBtn.setText("Normal Mode");
+        normalModeBtn.setSize(120,50);
+        normalModeBtn.setLocation(340,300);
+        add(normalModeBtn);
+        normalModeBtn.addActionListener(e -> {
+            gameFrame.setVisible(true);
+            this.setVisible(false);
+        });
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.addWindowListener(
+                new WindowAdapter()
+                {
+                    public void windowClosing(WindowEvent e)
+                    {
+                        setVisible(false);
+                        gameFrame.close();
+                    }
+                });
+    }
+
+    public void close(){
+        SecurityManager security = System.getSecurityManager();
+        if (security != null) {
+            security.checkExit(0);
+        }
+    }
+
+    public void setGameFrame(GameFrame gameFrame)
+    {
+        this.gameFrame = gameFrame;
+    }
+}

2c186c461bb41bf41d6f23073754b3dea21ac280
Crazyidiott <90187982+Crazyidiott@users.noreply.github.com>
2021-12-17 11:31:15 +0800

    add musicstuff

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import music.MusicStuff;
 import view.GameFrame;
 import view.MenuFrame;
 import model.ImageValue;
@@ -5,13 +6,14 @@ import model.ImageValue;
 import javax.swing.*;
 
 public class Main{
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         //TODO 结束时记得删掉所有可下子位置
-        ImageValue.init();
-        SwingUtilities.invokeLater(() -> {
-            MenuFrame menuFrame = new MenuFrame(500);
+         MusicStuff musicstuff = new MusicStuff("src/music/musicplay.wav");
+         ImageValue.init();
+         SwingUtilities.invokeLater(() -> {
+            MenuFrame menuFrame = new MenuFrame(500,musicstuff);
             menuFrame.setVisible(true);
-            GameFrame gameFrame=new GameFrame(700,menuFrame);
+            GameFrame gameFrame=new GameFrame(700,menuFrame,musicstuff);
             gameFrame.setVisible(false);
             menuFrame.setGameFrame(gameFrame);
         });
